6f5c22f4f1ceb16ebfaaad32c9892f8ffae89447
Sakib21029 <towhidujjaman.sakib@gmail.com>
2026-09-06 00:18:52 +0600

    Update root build.sh with full build workflow

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
+set -o errexit
 
+cd mmr_lab
+pip install -r requirements.txt
+python manage.py migrate
+
+echo "ADMIN SCRIPT STARTING"
+python create_admin.py
+
+python manage.py collectstatic --no-input
