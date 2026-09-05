ef38a853d56153aeba39f59e567ec715b5acae93
Sakib21029 <towhidujjaman.sakib@gmail.com>
2026-09-05 23:59:53 +0600

    Add executable build.sh script for Render

---
+++ build.sh
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py migrate
+
+echo "ADMIN SCRIPT STARTING"
+python create_admin.py
+
+python manage.py collectstatic --no-input
