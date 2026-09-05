44db7602a71a1045fa559a4cd977bbe04741055c
Sakib21029 <towhidujjaman.sakib@gmail.com>
2026-09-06 00:15:19 +0600

    Add root build.sh to enter mmr_lab and run build tasks

--- build.sh
+++ build.sh
@@ -1,11 +1,1 @@
-#!/usr/bin/env bash
-set -o errexit
 
-cd mmr_lab
-pip install -r requirements.txt
-python manage.py migrate
-
-echo "ADMIN SCRIPT STARTING"
-python create_admin.py
-
-python manage.py collectstatic --no-input
